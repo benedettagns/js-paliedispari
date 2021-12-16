c904549e342b1fff46695861c84c4b94d10b2b9b
benedettagns <benedetta.genise01@gmail.com>
2021-12-16 16:41:41 +0100

    correzioni

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const scelta = parseInt(prompt('Scegliere pari o dispari'));
+const scelta = parseInt(prompt('Scegliere pari o dispari (inserisci 0 per pari e 1 per dispari)'));
 const numero = parseInt(prompt('Inserire un numero da 1 a 5'));
 
 //funzione per numero random
@@ -7,12 +7,15 @@ function numeroRandom(minNum, maxNum) {
     return acaso;
 };
 
-const button = document.getElementById('button');
+let computerNumber = numeroRandom (1, 5);
+
+
+/*const button = document.getElementById('button');
 const div = document.getElementById('randomico');
 
 button.addEventListener('click', function () {
     let number = numeroRandom (1, 5);
     div.innerHTML = number;
-});
+});*/
 
 
