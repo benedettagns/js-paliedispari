4105d0256af5e05d71aa7af3fb59861935ed131c
benedettagns <benedetta.genise01@gmail.com>
2021-12-16 22:33:23 +0100

    somma

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const scelta = parseInt(prompt('Scegliere pari o dispari (inserisci 0 per pari e 1 per dispari)'));
-const numero = parseInt(prompt('Inserire un numero da 1 a 5'));
+let numero = parseInt(prompt('Inserire un numero da 1 a 5'));
+
+console.log(scelta);
+console.log(numero);
 
 //funzione per numero random
 function numeroRandom(minNum, maxNum) {
@@ -8,14 +11,28 @@ function numeroRandom(minNum, maxNum) {
 };
 
 let computerNumber = numeroRandom (1, 5);
+console.log(computerNumber);
+
+let risultato = computerNumber + numero;
+console.log(risultato)
+
+risultato = risultato * 1;
+if (risultato < 0) {
+    risultato = -risultato;
+} else if   (risultato > 1) {
+    risultato = risultato - 2;
+} 
+
+if (risultato==0) {
+    alert("Il numero è pari");
+} else {
+    alert("Il numero è dispari");
+}
 
 
-/*const button = document.getElementById('button');
-const div = document.getElementById('randomico');
 
-button.addEventListener('click', function () {
-    let number = numeroRandom (1, 5);
-    div.innerHTML = number;
-});*/
+
+
+
 
 
