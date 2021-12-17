d28b3ef5fef882f9e6c831c5bc765f2a241f17c8
benedettagns <benedetta.genise01@gmail.com>
2021-12-17 10:06:12 +0100

    correzione

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+//primo esercizio - non sono riuscita a farlo 
+
+//correzione fatta in classe 
+/* function isPalindroma(word) {
+    let reverse = '';
+    for (let i = word.lenght - 1; i >= 0; i--){
+        reverse += word[i]
+    }
+
+    if(reverse === word) {
+        return true;
+    } else {
+        return false;
+    }
+
+    //alternativa
+    if(reverse === word) {
+        return true;
+    }
+    return false;
+};
+const parolaUtente = prompt('Inserisci una parola');
+
+if (isPalindroma(parolaUtente)) {
+    console.log('è palindroma');
+} else {
+    console.log('NON è palindroma');
+}*/
+
+
+
+//secondo esercizio 
+
+
 const scelta = parseInt(prompt('Scegliere pari o dispari (inserisci 0 per pari e 1 per dispari)'));
 let numero = parseInt(prompt('Inserire un numero da 1 a 5'));
 
@@ -12,14 +46,16 @@ function numeroRandom(minNum, maxNum) {
 
 let computerNumber = numeroRandom (1, 5);
 console.log(computerNumber);
+document.getElementById('randomico').innerHTML = computerNumber;
 
 let risultato = computerNumber + numero;
-console.log(risultato)
+console.log(risultato);
+document.getElementById('somma-numeri').innerHTML = risultato;
 
 risultato = risultato * 1;
 if (risultato < 0) {
     risultato = -risultato;
-} else if   (risultato > 1) {
+} while  (risultato > 1) {
     risultato = risultato - 2;
 } 
 
